ffd609fd05cd9727e88742c6715427add8d75b73
Tydiggidy <lindsayt10@uww.edu>
2014-12-07 17:09:06 -0600

    Revert "Revert "test Row/Column""
    This reverts commit c2d6eeab62f1cc3cc85c7e25615c47bcd459fecd.

--- public/apiTest.php
+++ public/apiTest.php
@@ -17,7 +17,7 @@ if (isset($_GET['make']) && isset($_GET['model']) && isset($_GET['year'])) {
 ?>
 <!doctype html>
 <html>
-
+    <!-- bootstrap -->
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -25,29 +25,41 @@ if (isset($_GET['make']) && isset($_GET['model']) && isset($_GET['year'])) {
         <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    </head>
+    </head><!-- -->
 
     <body>
-        <form method="get">
-            <label>Make</label>
-            <input type="text" name="make" value="<?=$_GET['make']?>"/>
-            <label>Model</label>
-            <input type="text" name="model" value="<?=$_GET['model']?>"/>
-            <label>Year</label>
-            <input type="text" name="year" value="<?=$_GET['year']?>"/>
-            <input type="submit"/>
-        </form>
-        <hr/>
+        <div class= 'container'>
 
-        <?php if ($error != null):?>
-            <b>Error:</b> <?=$error?>
-        <?php endif;?>
+            <div class = 'row'>
 
-        <h2>Photos</h2>
-        <?php foreach ($photos as $photo):?>
-            <div><img style="max-width:100%" src="<?=$photo->getBestQualityUrl()?>"></div>
-        <?php endforeach;?>
+                <div class = 'col-md-12'>
 
-        <div>Got data in <?=$client->getRequestCount()?> API request(s).</div>
+                    <form method="get">
+                        <label>Make</label>
+                       <input type="text" name="make" value="<?=$_GET['make']?>"/>
+                        <label>Model</label>
+                        <input type="text" name="model" value="<?=$_GET['model']?>"/>
+                        <label>Year</label>
+                        <input type="text" name="year" value="<?=$_GET['year']?>"/>
+                        <input type="submit"/>
+                    </form>
+                    <hr/>
+
+                </div>
+                
+            </div>
+
+            <?php if ($error != null):?>
+                <b>Error:</b> <?=$error?>
+            <?php endif;?>
+
+            <h2>Photos</h2>
+            <?php foreach ($photos as $photo):?>
+                <div><img style="max-width:100%" src="<?=$photo->getBestQualityUrl()?>"></div>
+            <?php endforeach;?>
+
+            <div>Got data in <?=$client->getRequestCount()?> API request(s).</div>
+
+        </div>
     </body>
 </html>
