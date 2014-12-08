f46d1c5bfba4d25088942d456078b0fa5bae6b05
Tydiggidy <lindsayt10@uww.edu>
2014-12-08 12:05:01 -0600

    homepage

---
+++ public/home.php
@@ -0,0 +1,34 @@
+<!doctype html>
+
+<html>
+    <!-- bootstrap -->
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="/bootstrap/3.2.0/css/bootstrap.min.css">
+
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+
+        <style>
+            .banner { background-color: #2F4F4F; color: #fff; height: 100px;}
+            .footer { background-color: #2F4F4F; color: #fff; height: 50px; }
+
+        </style>
+    </head><!-- -->
+
+    <body>
+        <div class= 'container'>
+
+            <div class='row banner'>
+                <div class='col-md-12'>
+                    <h1>Home!</h1>
+                </div>
+            </div>
+            
+            <div class = 'row footer'>
+            	<div class = 'col-md-12'></div>
+            </div>
+    </body>
+
+</html>
