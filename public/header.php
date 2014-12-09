1c7af5ab29911a8a76fa76537a23c503a61b6855
coderstephen <me@stephencoakley.com>
2014-12-09 01:32:46 -0600

    Fixed bootstrap URL

--- public/header.php
+++ public/header.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="stylesheet" href="/bootstrap/3.2.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
 
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 
