d96659698c32e527b537ed80b6e5dd5debe748f1
coderstephen <me@stephencoakley.com>
2014-12-09 01:10:23 -0600

    Ref update

--- public/index.php
+++ public/index.php
@@ -1,11 +1,11 @@
 <?php
-<<<<<<< HEAD
-echo "Car-Rived";
-
+    $action = "";
+    if(isset($_REQUEST['action']))
+        $action = $_REQUEST['action'];
+    switch($action){
+        case "test": include("./apiTest.php");
+                break;
+        default: include("./home.php");
+                break;
+    }
 ?>
-=======
-echo "Hello";
-echo "Hi";
-
-?>
->>>>>>> origin/master
