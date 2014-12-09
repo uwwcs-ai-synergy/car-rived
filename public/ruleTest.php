de825ed974a5c9cce26a4d3b51149f69c2a41ccb
coderstephen <me@stephencoakley.com>
2014-12-09 01:11:52 -0600

    Removed unnecessary junk

--- public/ruleTest.php
+++ public/ruleTest.php
@@ -2,11 +2,18 @@
 namespace CarRived;
 require '../src/Autoloader.php';
 
-$api = new Edmunds\VehicleApiClient('ryxpfa645s3vc4vywcwkdju9');
+$cache = new Edmunds\ApiCache('../cache');
+$api = new Edmunds\VehicleApiClient('ryxpfa645s3vc4vywcwkdju9', $cache);
 
-$style = $api->getModelStyles('volkswagen', 'golf', 2000)[0];
-
+$style = $api->getModelStyles('volkswagen', 'golf', 2014)[0];
+//var_dump($style);
 $rules = new RuleSet();
-$rules->addRule(new AgeRule(2009));
+$rules->addRule(new AgeRule(2000));
+$rules->addRule(new PriceRangeRule(10000, 30000));
 
-echo $rules->heuristic($style);
+var_dump($rules->heuristic($style));
+
+
+$searcher = new VehicleSearcher($api, $rules);
+var_dump($searcher->search('mini', 9));
+var_dump($api->getRequestCount());
