2ace2f65fb858a510f34bcee4d2e9e9bc2244e3a
coderstephen <me@stephencoakley.com>
2014-12-09 02:32:39 -0600

    Split into min and max price

--- public/rulePage.php
+++ public/rulePage.php
@@ -31,32 +31,38 @@
             <?php include("./links.php");?>
 
                 <div class = 'col-md-9'>
-                        
+
                     <form method = 'get' action = 'result.php'>
                         <div class="input-group">
                             <span class="input-group-addon">Make</span>
                             <input type="text" class="form-control" placeholder="ex.volkswagen" name="make" value=""/>
                         </div>
-                        
+
                         <div class="input-group">
                             <span class="input-group-addon">Year</span>
                             <input type="text" class="form-control" placeholder="ex.2000" name="year" value=""/>
                         </div>
 
                         <div class="input-group">
-                            <span class="input-group-addon">Price</span>
-                            <input type="text" class="form-control" placeholder="ex.5000" name="price" value=""/>
+                            <span class="input-group-addon">Min Price</span>
+                            <input type="text" class="form-control" placeholder="ex.20000" name="minPrice" value=""/>
                         </div>
+
+                        <div class="input-group">
+                            <span class="input-group-addon">Max Price</span>
+                            <input type="text" class="form-control" placeholder="ex.30000" name="maxPrice" value=""/>
+                        </div>
+
                         <div>
                             <button class = 'btn btn-primary' type = 'submit'>Submit
                                 <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
                             </button>
                         </div>
-                       
+
                     </form>
                     <hr/>
-                    
-                </div>    
+
+                </div>
             </div>
 
              <div class='row footer'>
@@ -64,6 +70,6 @@
             </div>
 
         </div>
-        
+
     </body>
 </html>
